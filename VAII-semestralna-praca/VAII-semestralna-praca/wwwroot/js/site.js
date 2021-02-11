82552bcfa12645776f5e91c959b4718f20ee64e3
birwen <farkasova.bibiana@gmail.com>
2021-02-11 15:53:00 +0100

    pridanie funkcionalít do básní preklad do slovenčiny načítanie súboru (js)

--- VAII-semestralna-praca/VAII-semestralna-praca/wwwroot/js/site.js
+++ VAII-semestralna-praca/VAII-semestralna-praca/wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿(function () {
+	if (!localStorage.getItem('cookieconsent')) {
+		document.body.innerHTML += '\
+		<div class="cookieconsent" style="position:fixed;padding:20px;left:0;bottom:0;background-color:#000;color:#FFF;text-align:center;width:100%;z-index:99999;">\
+			This site uses cookies. By continuing to use this website, you agree to their use. \
+			<a href="#" style="color:#CCCCCC;">I Understand</a>\
+		</div>\
+		';
+		document.querySelector('.cookieconsent a').onclick = function (e) {
+			e.preventDefault();
+			document.querySelector('.cookieconsent').style.display = 'none';
+			localStorage.setItem('cookieconsent', true);
+		};
+	}
+});
 
-// Write your Javascript code.
+function readSingleFile(e) {
+	var file = e.target.files[0];
+	if (!file) {
+		return;
+	}
+	var reader = new FileReader();
+	reader.onload = function (e) {
+		var contents = e.target.result;
+		// Display file content
+		displayContents(contents);
+	};
+	reader.readAsText(file);
+}
+
+function displayContents(contents) {
+	var element = document.getElementById('file-content');
+	element.setAttribute('value', contents);
+}
+
+document.getElementById('file-input').addEventListener('change', readSingleFile, false);
